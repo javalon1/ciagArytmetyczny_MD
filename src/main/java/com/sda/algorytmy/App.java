142dc643ca59c659662ef6afe8c44309054edd6e
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-18 11:34:53 +0100

    zrobione

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,32 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwszy wyraz ciagu: ");
+        int a = scanner.nextInt();
+        System.out.println("Podaj ilosc wyrazow ciagu: ");
+        int n = scanner.nextInt();
+        System.out.println("Podaj przyrost ciagu: ");
+        int d = scanner.nextInt();
+        System.out.print(a + ", ");
+        int next;
+
+        for(int i = 2; i <= n; i++){
+            next = a + (i - 1) * d;
+            System.out.print(next + ", ");
+        }
     }
 }
+
+
+// n – liczba wyrazów ciągu do wygenerowania
+
+// a – pierwszy wyraz ciągu,
+
+// d – przyrost     a = a + (i - 1) x d
